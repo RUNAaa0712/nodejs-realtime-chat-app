839f79988f81ab26e955357b237635ed791fd148
RUNAaa0712 <98579097+tjc-kousei@users.noreply.github.com>
2022-07-01 16:05:00 +0900

    PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -19,5 +19,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(PORT, () => {
-    console.log("listening on 3000");
+    console.log("listening on "+ PORT);
 });
